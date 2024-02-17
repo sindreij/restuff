721c10f958fd1b08c8e45c16f50a2e79c0583638
Sindre Ilebekk Johansen <sindre@sindrejohansen.no>
2024-02-17 12:44:47 +0100

    A really bare-bones implementation

--- restuff-api/src/main.rs
+++ restuff-api/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+mod srpc;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/srpc/:call", get(srpc::handle_get));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
